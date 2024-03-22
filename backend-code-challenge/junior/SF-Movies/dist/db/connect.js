504de76edd683f5decaa2bdbc5919f427f4616ee
mayaclaudia <mayaammuclaudia@gmail.com>
2024-03-22 03:10:06 -0700

    update dist

--- backend-code-challenge/junior/SF-Movies/dist/db/connect.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/task-manager';
-exports.default = uri;
